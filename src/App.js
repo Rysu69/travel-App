87e697b0bec1d42943348095cb801064ce87d4db
Rysu69 <141984337+Rysu69@users.noreply.github.com>
2024-02-12 11:18:58 +0800

    done coyy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+// const initialItems = [
+//     { id: 1, description: "Passport", quantity: 2, packed: false},
+//     { id: 2, description: "Socks", quantity: 12, packed: true},
+//     { id: 3, description: "Power Bank", quantity: 1, packed: true}
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
+      <Stats />
     </div>
   );
+} 
+
+function Logo() {
+    return <h1> JALAN KUY</h1>;
 }
 
-export default App;
+function Form({ onAddItems }) { 
+
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+}
+
+    return (
+        <form className="add-form" onSubmit={handleSubmit}>
+            <h3>Apa aja yang dibawa?</h3>
+            <h3>Yuk Checklist Barang</h3>
+            <select
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+            >
+                {Array.from({ length: 20}, (_, i) => i + 1).map((num) => (
+                    <option value={num}>{num}</option>
+                ))}
+            </select>
+            <input type="text" 
+            placeholder="Barang yang mau dibawa"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            />
+            <button>Bawa</button>
+        </form>
+    );
+}
+
+function PackingList({ items }) {
+    return(
+       <div className="list">
+        <ul>
+            {items.map((item) => (
+                <Item item={item} key={item.id}/>
+            ))}
+        </ul>
+    </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            {/* ternary operator to check simple condition */}
+            {/* if item.packed === true then apply this style textDecoration: "line-through" 
+            else don't do anything */}
+            <span style={item.packed ? { textDecoration: "line-through"} : {}}>
+            {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
+}
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <em>
+                Kamu punya 0 barang di daftar, dan sudah packing 0 barang (0%){" "}
+            </em>
+        </footer>
+    );
+}
